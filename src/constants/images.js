83a6373b1e9d493c5cb74fa6ecc83cb31e697efa
TomisinAjayi <thomieajayi@gmail.com>
2023-11-01 19:55:28 +0100

    15 images only

--- src/constants/images.js
+++ src/constants/images.js
@@ -7,24 +7,12 @@ const image6 = require('../assets/images/eduardo-espinoza-laXkboVd860-unsplash.j
 const image7 = require('../assets/images/eduardo-espinoza-rl7jMcSUnSg-unsplash.jpg');
 const image8 = require('../assets/images/johnnathan-tshibangu-YV3NiD_lEwo-unsplash.jpg');
 const image9 = require('../assets/images/kotryna-juskaite-GnQhaK9b6sc-unsplash.jpg');
-const image10 = require('../assets/images/23-studiio-OWdC2lDjj40-unsplash.jpg');
-const image20 = require('../assets/images/23-studiio-p7rdghH5xWY-unsplash.jpg');
-const image30 = require('../assets/images/atiyeh-fathi-iVYFTxHD5W4-unsplash.jpg');
-const image40 = require('../assets/images/davide-manzini-x9tXh7SAauI-unsplash.jpg');
-const image50 = require('../assets/images/donna-elliot-Jl5vroEE7Ik-unsplash.jpg');
-const image60 = require('../assets/images/eduardo-espinoza-laXkboVd860-unsplash.jpg');
-const image70 = require('../assets/images/eduardo-espinoza-rl7jMcSUnSg-unsplash.jpg');
-const image80 = require('../assets/images/johnnathan-tshibangu-YV3NiD_lEwo-unsplash.jpg');
-const image90 = require('../assets/images/kotryna-juskaite-GnQhaK9b6sc-unsplash.jpg');
-const image100 = require('../assets/images/23-studiio-OWdC2lDjj40-unsplash.jpg');
-const image200 = require('../assets/images/23-studiio-p7rdghH5xWY-unsplash.jpg');
-const image300 = require('../assets/images/atiyeh-fathi-iVYFTxHD5W4-unsplash.jpg');
-const image400 = require('../assets/images/davide-manzini-x9tXh7SAauI-unsplash.jpg');
-const image500 = require('../assets/images/donna-elliot-Jl5vroEE7Ik-unsplash.jpg');
-const image600 = require('../assets/images/eduardo-espinoza-laXkboVd860-unsplash.jpg');
-const image700 = require('../assets/images/eduardo-espinoza-rl7jMcSUnSg-unsplash.jpg');
-const image800 = require('../assets/images/johnnathan-tshibangu-YV3NiD_lEwo-unsplash.jpg');
-const image900 = require('../assets/images/kotryna-juskaite-GnQhaK9b6sc-unsplash.jpg');
+const image10 = require('../assets/images/nice-m-nshuti-iZFN-CjqZ7o-unsplash.jpg');
+const image11 = require('../assets/images/ahmed-EDVZq9RcQpQ-unsplash.jpg');
+const image12 = require('../assets/images/ahmed-8puyVUFin10-unsplash.jpg');
+const image13 = require('../assets/images/teslariu-mihai-o-jOTmSjoLY-unsplash.jpg');
+const image14 = require('../assets/images/teslariu-mihai-IKo8qX7a-hA-unsplash.jpg');
+const image15 = require('../assets/images/tom-morbey-TcW0vtTR1uo-unsplash.jpg');
 
 const images = {
   image1,
@@ -37,23 +25,11 @@ const images = {
   image8,
   image9,
   image10,
-  image20,
-  image30,
-  image40,
-  image50,
-  image60,
-  image70,
-  image80,
-  image90,
-  image100,
-  image200,
-  image300,
-  image400,
-  image500,
-  image600,
-  image700,
-  image800,
-  image900,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
 };
 
 export default images;
